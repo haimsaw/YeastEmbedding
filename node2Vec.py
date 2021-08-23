404dab0d358111b39b2f52de079bfe0c57fd0648
Haim Sawdayee <haimsaw@gmail.com>
2021-08-24 00:46:18 +0300

    refactor

--- node2Vec.py
+++ node2Vec.py
@@ -9,26 +9,35 @@ from clusterUtils import *
 from graphUtils import *
 import networkx as nx
 
-from sklearn.cluster import AffinityPropagation
-
+from sklearn.cluster import AffinityPropagation, MiniBatchKMeans
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def train(model, optimizer, loader):
-    model.train()
-    total_loss = 0
-    for pos_rw, neg_rw in loader:
-        optimizer.zero_grad()
-        loss = model.loss(pos_rw.to(device), neg_rw.to(device))
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(loader)
+# region general
 
+def get_data():
+    G = nx.read_edgelist('db/huri_symbol.tsv')
+    G.remove_edges_from(nx.selfloop_edges(G))
+    G = nx.k_core(G, 3)  # todo hyperparam
+    data = from_networkx(G)
+
+    # n_train = int(len(data) * .8)
+    # idx = torch.randperm(len(data))
+    # data.train_mask = idx[:n_train]
+    # data.test_mask = idx[n_train:]
+
+    gaf_data = readGafFile("./db/goa_human.gaf")
+
+    return data, G, gaf_data
+
+#TODO getProteinAnnotation IS SLOW nodeRowIdx = proteinsList.index(node)
+# endregion
+
+# region plotting
 
 @torch.no_grad()
-def plot_2d_embbedings(model, data):
+def plot_2d_embeddings(model, data):
     model.eval()
     z = model(torch.arange(data.num_nodes, device=device))
     z = TSNE(n_components=2).fit_transform(z.cpu().numpy())
@@ -41,30 +50,14 @@ def plot_2d_embbedings(model, data):
 
 
 @torch.no_grad()
-def plot_2d_embbedings_with_lable(embbeded, labels):
-    z = TSNE(n_components=2).fit_transform(embbeded)
+def plot_2d_embeddings_with_label(embedded, labels):
+    z = TSNE(n_components=2).fit_transform(embedded)
     print(z.shape)
 
     plt.figure(figsize=(8, 8))
-    for i in range(max(labels)+1):
+    for i in range(max(labels) + 1):
         plt.scatter(z[labels == i, 0], z[labels == i, 1], s=20)
     plt.show()
-
-
-def get_data():
-    G = nx.read_edgelist('db/huri_symbol.tsv')
-    G.remove_edges_from(nx.selfloop_edges(G))
-    G = nx.k_core(G, 3)  # todo hyperparam
-    data = from_networkx(G)
-
-    n_train = int(len(data) * .8)
-    idx = torch.randperm(len(data))
-    data.train_mask = idx[:n_train]
-    data.test_mask = idx[n_train:]
-
-    gaf_data = readGafFile("./db/goa_human.gaf")
-
-    return data, G, gaf_data
 
 
 @torch.no_grad()
@@ -72,65 +65,9 @@ def plot_each_cluster(G, labels):
     for i in range(max(labels) + 1):
         nodes = np.array(G.nodes)[labels == i]
         H = G.subgraph(nodes)
-        print(f'\n\ncluser={i}, n_nodes={len(nodes)}:')
+        print(f'\n\ncluster={i}, n_nodes={len(nodes)}:')
         nx.draw(H, node_size=70)
         plt.show()
-
-
-def cluster_affinity_propagation(embedded, preference, damping):
-    # preference - controls how many exemplars are used
-    # damping factor - damps the responsibility and availability messages (between 0.5 and 1)
-    af = AffinityPropagation(preference=preference, damping=damping).fit(embedded)
-    labels = af.labels_
-    n_clusters_ = len(np.unique(labels))
-    print(f'Number of clusters: {n_clusters_}')
-    return labels
-
-
-def cluster_embeddings(G, embedded, gaf_data, preference, damping):
-    affinity_propagation_labels = cluster_affinity_propagation(embedded, preference=preference, damping=damping)
-    indices = np.array(range(len(affinity_propagation_labels)))
-
-    affinity_propagation_clusters = []
-    for i in range(max(affinity_propagation_labels)+1):
-        affinity_propagation_clusters.append(indices[affinity_propagation_labels == i])
-
-    # plot_each_cluster(G, affinity_propagation_labels)
-    # plot_2d_embbedings_with_lable(embbeded, affinity_propagation_labels)
-    return get_partition_score(G, affinity_propagation_clusters, gaf_data)
-
-
-def embed(data, epochs, p, q, embedding_dim, walk_length, walks_per_node):
-    model = Node2Vec(data.edge_index, embedding_dim=embedding_dim, walk_length=walk_length,
-                     context_size=10, walks_per_node=walks_per_node,
-                     num_negative_samples=1, p=p, q=q, sparse=True).to(device)
-    # todo we want more bfs - homophily
-    # p = return parameter,  likelihood of immediately revisiting a node
-    # q = in-out parameter, if q > 1, the random walk is biased towards nodes close to node t. (more bfs)
-
-    loader = model.loader(batch_size=128, shuffle=True, num_workers=4)
-    optimizer = torch.optim.SparseAdam(list(model.parameters()), lr=0.01)
-
-    losses = []
-    for epoch in range(epochs):
-        loss = train(model, optimizer, loader)
-        losses.append(loss)
-        if epoch % 10 == 0:
-            print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
-
-    plt.bar(range(len(losses)), losses)
-    plt.show()
-
-    model.eval()
-    return model(torch.arange(data.num_nodes, device=device)).cpu().detach().numpy(), losses[-1]
-
-
-def run_test(G, data,  gaf_data, epochs, p, q, embedding_dim, walk_length, walks_per_node, preference, damping):
-    embedded, embedding_loss = embed(data, epochs=epochs, p=p, q=q, embedding_dim=embedding_dim, walk_length=walk_length, walks_per_node=walks_per_node)
-    score = cluster_embeddings(G, embedded, gaf_data, preference=preference, damping=damping)
-    print(f'q={q} p={p} embedding_loss={embedding_loss} score={score}\n\n')
-
-    return embedding_loss, score
 
 
 def show_exp_results(ps, qs, scores):
@@ -148,24 +85,136 @@ def show_exp_results(ps, qs, scores):
     plt.show()
 
 
+# endregion
+
+
+# region embedding
+
+
+def train(model, optimizer, loader):
+    model.train()
+    total_loss = 0
+    for pos_rw, neg_rw in loader:
+        optimizer.zero_grad()
+        loss = model.loss(pos_rw.to(device), neg_rw.to(device))
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(loader)
+
+
+def embed(data, epochs, p, q, embedding_dim, walk_length, walks_per_node):
+    model = Node2Vec(data.edge_index, embedding_dim=embedding_dim, walk_length=walk_length,
+                     context_size=10, walks_per_node=walks_per_node,
+                     num_negative_samples=1, p=p, q=q, sparse=True).to(device)
+    # todo we want more bfs - homophily
+    # p = return parameter,  likelihood of immediately revisiting a node
+    # q = in-out parameter, if q > 1, the random walk is biased towards nodes close to node t. (more bfs)
+
+    loader = model.loader(batch_size=128, shuffle=True, num_workers=4)
+    optimizer = torch.optim.SparseAdam(list(model.parameters()), lr=0.01)
+
+    losses = [0]
+    for epoch in range(epochs):
+        loss = train(model, optimizer, loader)
+        losses.append(loss)
+        if epoch % 10 == 0:
+            print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
+
+    plt.bar(range(len(losses)), losses)
+    plt.show()
+
+    model.eval()
+    return model(torch.arange(data.num_nodes, device=device)).cpu().detach().numpy(), losses[-1]
+
+
+# endregion
+
+
+# region clustering
+
+def cluster_embeddings(G, embedded, gaf_data, clustering_alg, **kwargs):
+    if clustering_alg == "affinity_propagation":
+        # preference - controls how many exemplars are used
+        # damping factor - damps the responsibility and availability messages (between 0.5 and 1)
+        labels = AffinityPropagation(preference=kwargs["preference"], damping=kwargs["damping"]).fit(embedded).labels_
+
+    elif clustering_alg == "k_means":
+        labels = MiniBatchKMeans(n_clusters=kwargs["n_clusters"], batch_size=kwargs["batch_size"]).fit(embedded).labels_
+
+    else:
+        assert False
+
+    indices = np.array(range(len(labels)))
+    clusters = []
+    for i in range(max(labels) + 1):
+        clusters.append(indices[labels == i])
+
+    n_clusters_ = len(np.unique(labels))
+    print(f'Number of clusters: {n_clusters_}')
+
+    # plot_each_cluster(G, affinity_propagation_labels)
+    # plot_2d_embbedings_with_lable(embbeded, affinity_propagation_labels)
+    return get_partition_score(G, clusters, gaf_data)
+
+
+# endregion
+
+
+# region hyperparams
+
+def parse_hyperparams(hyperparams):
+    embedding_hyperparams = {
+        "epochs": 0,
+        "p": hyperparams[0],
+        "q": hyperparams[1],
+        "embedding_dim": 128,
+        "walk_length": 20,
+        "walks_per_node": 10
+    }
+
+    clustering_hyperparams = {
+        "clustering_alg": "affinity_propagation",
+        "preference": None,
+        "damping": 0.5
+    }
+
+    clustering_hyperparams = {
+        "clustering_alg": "k_means",
+        "n_clusters": 100,
+        "batch_size": 100
+    }
+    return embedding_hyperparams, clustering_hyperparams
+
+
+def run_test(G, data, gaf_data, embedding_hyperparams, clustering_hyperparams):
+    embedded, embedding_loss = embed(data, **embedding_hyperparams)
+    score = cluster_embeddings(G, embedded, gaf_data, **clustering_hyperparams)
+    print(f'embedding_hyperparams={embedding_hyperparams} clustering_hyperparams={clustering_hyperparams} embedding_loss={embedding_loss:.4f} score={score:.4f}\n\n')
+
+    return embedding_loss, score
+
+
+# endregion
+
+
 def main():
     data, G, gaf_data = get_data()
-    #nx.draw(G)
-    #plt.show()
-
-    embedded, embedding_loss = embed(data, epochs=50, p=1, q=1, embedding_dim=128, walk_length=20, walks_per_node=10)
-    score = cluster_embeddings(G, embedded, gaf_data, preference=None, damping=0.5)
-    print(f'sacore={score}')
+    # nx.draw(G)
+    # plt.show()
 
     ps = np.linspace(0.1, 4, 5)
     qs = np.linspace(0.1, 4, 5)
     test_matrix = np.stack(np.meshgrid(ps, qs), axis=-1)
 
-    res = np.apply_along_axis(lambda x: run_test(G, data,  gaf_data, epochs=50, p=x[0], q=x[1], embedding_dim=128, walk_length=20, walks_per_node=10, preference=None, damping=0.5), -1, test_matrix)
+    res = np.apply_along_axis(
+        lambda hyperparams: run_test(G, data, gaf_data, *parse_hyperparams(hyperparams)),
+        -1, test_matrix)
 
     embedding_loss, scores = res[:, :, 0], res[:, :, 1]
 
     show_exp_results(ps, qs, scores)
+
 
 if __name__ == "__main__":
     main()
